06a0d815760e7071c96791ac30aa176dd279a2f2
orange <orange@unknownemail.com>
2023-03-21 18:50:15 +0800

    [FEAT] Add a basic structure
    Signed-off-by: orange <orange@unknownemail.com>

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+from settings import *
+from level import Level
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Stardew Valley')
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000 # Counts the time that has elapsed since the last call to the function
+            self.level.run(dt)
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
