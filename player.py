edd258d87621841b71f1f8085bf29d41553b5e05
orange <orange@unknownemail.com>
2023-03-23 19:00:57 +0800

    [FEAT] create an animation player
    Signed-off-by: orange <orange@unknownemail.com>

--- player.py
+++ player.py
@@ -1,13 +1,17 @@
 import pygame
 from settings import *
+from support import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
         # general setup
-        self.image = pygame.Surface((32, 64)) # a surface obj
-        self.image.fill('green')
+        self.image = self.animations[self.status][self.frame_index] # a surface obj
         self.rect = self.image.get_rect(center = pos) # get rectangle area from surface
 
         # movement attributes
@@ -15,22 +19,62 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center) # to update rect
         self.speed = 200 # move speed
 
+        # tools
+        self.selected_tool = 'axe'
+
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                           'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
+                           'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
+                           'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []}
+
+        for animation in self.animations.keys():
+            # can use pathlib module
+            full_path = './graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+        self.image = self.animations[self.status][int(self.frame_index)]
+
+
     def input(self):
         keys = pygame.key.get_pressed()
-
+        
+        # directions
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
+
+        # tool use
+         
+
+    def get_status(self):
+
+        # idle
+        if self.direction.magnitude() == 0:
+            # if player is not moving
+            # status should be _idle
+            self.status = self.status.split('_')[0] + '_idle'
+
+        # tool use
 
     def move(self, dt):
         if self.direction.magnitude() > 0:
@@ -46,4 +90,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
